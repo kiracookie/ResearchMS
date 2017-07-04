fc0145cfe1cd3b18b87e85d94081e5ab8dfae1dc
kiracookie <kira0109@qq.com>
2017-07-04 22:59:55 +0800

    papers列表

--- app/src/main/java/com/cookie/kira/researchms/activity/MainActivity.java
+++ app/src/main/java/com/cookie/kira/researchms/activity/MainActivity.java
@@ -2,9 +2,11 @@ package com.cookie.kira.researchms.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
 import com.cookie.kira.researchms.R;
+import com.cookie.kira.researchms.adapter.PaperAdapter;
 import com.cookie.kira.researchms.entity.Paper;
 
 import java.util.ArrayList;
@@ -13,23 +15,28 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
     private List<Paper> papers;
     private RecyclerView papaerListView;
-    private List<String> p;
+    private PaperAdapter paperAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.activity_main);
+
         papers = new ArrayList<>();
         Paper paperTest = new Paper();
         paperTest.setName("2016测试");
         papers.add(paperTest);
 
+        paperTest = new Paper();
         paperTest.setName("2017测试");
         papers.add(paperTest);
 
-
+        paperAdapter = new PaperAdapter(papers);
         papaerListView = (RecyclerView) findViewById(R.id.paperList);
-//        papaerListView.setAdapter();
+        papaerListView.setLayoutManager(new LinearLayoutManager(this));
+        papaerListView.setAdapter(paperAdapter);
 
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
     }
 }
--- app/src/main/java/com/cookie/kira/researchms/adapter/PaperAdapter.java
+++ app/src/main/java/com/cookie/kira/researchms/adapter/PaperAdapter.java
@@ -1,8 +1,55 @@
 package com.cookie.kira.researchms.adapter;
 
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.cookie.kira.researchms.R;
+import com.cookie.kira.researchms.activity.MainActivity;
+import com.cookie.kira.researchms.entity.Paper;
+
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Created by 11669 on 2017-06-29.
  */
 
-public class PaperAdapter {
+public class PaperAdapter extends RecyclerView.Adapter<PaperAdapter.PaperViewHolder> {
+    private List<Paper> papers = new LinkedList<>();
+
+    public PaperAdapter(List<Paper> papers) {
+        this.papers = papers;
+
+    }
+
+    @Override
+    public PaperAdapter.PaperViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        PaperViewHolder holder = new PaperViewHolder(LayoutInflater.from(
+                parent.getContext()).inflate(R.layout.item_home, parent,
+                false));
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(PaperAdapter.PaperViewHolder holder, int position) {
+        holder.tv.setText(papers.get(position).getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return papers.size();
+    }
+
+    public class PaperViewHolder extends RecyclerView.ViewHolder {
+        TextView tv;
+
+        public PaperViewHolder(View view) {
+            super(view);
+            tv = (TextView) view.findViewById(R.id.paper_item_title);
+        }
+    }
 }
